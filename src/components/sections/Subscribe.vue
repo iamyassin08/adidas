ead5e6f8276132106d141e5a2357bcbd9fb09f08
iamyassin08 <yassinmohamed49@gmail.com>
2024-07-20 13:44:57 -0500

    added on

--- src/components/sections/Subscribe.vue
+++ src/components/sections/Subscribe.vue
@@ -3,7 +3,7 @@ import Button from "../Button.vue";
   </script>
   
   <template>
-    <section class="padding-x sm:py-32 py-16 w-full"><section id="contact-us" class="max-container flex justify-between items-center max-lg:flex-col gap-10"><h3 class="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up for<span class="text-coral-red"> Updates </span>&amp; Newsletter</h3><div class="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full"><input type="text" placeholder="subscribe@nike.com" class="input"><div class="flex max-sm:justify-end items-center max-sm:w-full"><button class="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+    <section class="padding-x sm:py-32 py-16 w-full"><section id="contact-us" class="max-container flex justify-between items-center max-lg:flex-col gap-10"><h3 class="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up for<span class="text-coral-red"> Updates </span>&amp; Newsletter</h3><div class="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full"><input type="text" placeholder="subscribe@adidas.com" class="input"><div class="flex max-sm:justify-end items-center max-sm:w-full"><button class="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
       bg-coral-red text-white border-coral-red rounded-full w-full">Sign Up</button></div></div></section></section>
   </template>
 
