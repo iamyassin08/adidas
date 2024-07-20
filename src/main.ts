8de2be72be8222aae52596dcc6609c7ee5aee96a
iamyassin08 <yassinmohamed49@gmail.com>
2024-07-20 10:37:57 -0500

    added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-// import '@/style.css';
+import './style.css'
 import "preline/preline";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import './style.css'
+
 
 
 
