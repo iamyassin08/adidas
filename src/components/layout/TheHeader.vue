bcec95d617fe08b722d2e4b1653b807ae95d6da3
iamyassin08 <yassinmohamed49@gmail.com>
2024-07-20 23:00:11 -0500

    add

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -24,42 +24,46 @@ export default defineComponent({
 </script>
 
 <template>
-  <header class="absolute z-10 w-full py-8">
+  <header :class="{'bg-white dark:bg-gray-800': true}" class="absolute z-10 w-full py-8 transition-colors duration-300">
     <nav class="flex justify-between items-center max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 w-full z-0">
       <!-- Logo -->
       <RouterLink to="/" class="flex items-center">
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Adidas_2022_logo.svg"
           alt="logo"
-          class="w-[129px] h-[29px]"
+          class="w-[129px] h-[29px] transition-transform duration-300"
         />
         <h2 class="ml-2 font-bold text-blue-500 text-2xl">ADIDAS</h2>
       </RouterLink>
 
       <!-- Navigation Links -->
       <ul class="hidden lg:flex flex-1 justify-center items-center gap-8">
-        <li><RouterLink to="#home" class="text-lg text-gray-700 hover:text-blue-600">Home</RouterLink></li>
-        <li><RouterLink to="#about-us" class="text-lg text-gray-700 hover:text-blue-600">About Us</RouterLink></li>
-        <li><RouterLink to="#products" class="text-lg text-gray-700 hover:text-blue-600">Products</RouterLink></li>
-        <li><RouterLink to="#contact-us" class="text-lg text-gray-700 hover:text-blue-600">Contact Us</RouterLink></li>
+        <li><RouterLink to="#home" class="text-lg text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400">Home</RouterLink></li>
+        <li><RouterLink to="#about-us" class="text-lg text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400">About Us</RouterLink></li>
+        <li><RouterLink to="#products" class="text-lg text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400">Products</RouterLink></li>
+        <li><RouterLink to="#contact-us" class="text-lg text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400">Contact Us</RouterLink></li>
       </ul>
 
       <!-- Sign in and Explore Links -->
       <div class="hidden lg:flex gap-4 text-lg font-medium">
-        <RouterLink to="/" class="text-gray-700 hover:text-blue-600">Sign in</RouterLink>
+        <RouterLink to="/" class="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400">Sign in</RouterLink>
         <span>/</span>
-        <RouterLink to="/" class="text-gray-700 hover:text-blue-600">Explore now</RouterLink>
+        <RouterLink to="/" class="text-gray-700 hover:text-blue-600 dark:text-gray-300 dark:hover:text-blue-400">Explore now</RouterLink>
       </div>
 
       <!-- Dark mode and Mobile Menu Button -->
-      <div class="flex items-center gap-4">
+      <div class="flex items-center gap-x-4">
+        <!-- Horizontal Line -->
+        <hr class="hidden lg:block border-gray-300 dark:border-gray-700 mx-4 transition-colors duration-300" />
+
+        <!-- Dark Mode Toggle -->
         <button
           @click="toggleDarkMode"
           type="button"
-          class="hidden hs-dark-mode-active:block text-gray-600 hover:text-red-600 dark:text-neutral-400 dark:hover:text-neutral-500"
+          class="hidden hs-dark-mode-active:block text-gray-600 hover:text-red-600 dark:text-neutral-400 dark:hover:text-neutral-500 transition-colors duration-300"
           :class="{ hidden: !isDarkModeActive() }"
         >
-          <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+          <svg class="w-6 h-6 transition-transform duration-300" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
             <circle cx="12" cy="12" r="4"></circle>
             <path d="M12 2v2"></path>
             <path d="M12 20v2"></path>
@@ -74,10 +78,10 @@ export default defineComponent({
         <button
           @click="toggleDarkMode"
           type="button"
-          class="block hs-dark-mode-active:hidden text-gray-600 hover:text-red-600 dark:text-neutral-400 dark:hover:text-neutral-500"
+          class="block hs-dark-mode-active:hidden text-gray-600 hover:text-red-600 dark:text-neutral-400 dark:hover:text-neutral-500 transition-colors duration-300"
           :class="{ hidden: isDarkModeActive() }"
         >
-          <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
+          <svg class="w-6 h-6 transition-transform duration-300" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2">
             <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
           </svg>
         </button>
@@ -89,7 +93,7 @@ export default defineComponent({
           <img
             src="../../assets/icons/hamburger.svg"
             alt="hamburger menu icon"
-            class="w-6 h-6"
+            class="w-6 h-6 transition-transform duration-300"
           />
         </button>
       </div>
@@ -97,26 +101,26 @@ export default defineComponent({
 
     <!-- Mobile Menu -->
     <transition
-      enter-active-class="transition-opacity duration-500 transform"
+      enter-active-class="transition-opacity duration-500 ease-out"
       enter-class="opacity-0 -translate-x-10"
-      leave-active-class="transition-opacity duration-500 transform"
-      leave-to-class="opacity-0 -translate-x-10"
+      leave-active-class="transition-opacity duration-500 ease-in"
+      leave-to-class="opacity-0 translate-x-10"
     >
       <div
         v-show="isMobileMenuOpen"
-        class="fixed inset-0 bg-white shadow-lg z-20 lg:hidden flex flex-col items-center py-8 space-y-4 transform transition-transform"
+        class="fixed inset-0 bg-white shadow-lg dark:bg-gray-800 z-20 lg:hidden flex flex-col items-center py-8 space-y-4 transform transition-transform"
         @click.self="toggleMobileMenu"
       >
         <nav class="flex flex-col items-center w-full space-y-4">
-          <RouterLink to="#home" class="text-lg text-gray-700 py-2 w-full text-center hover:bg-gray-100">Home</RouterLink>
-          <RouterLink to="#about-us" class="text-lg text-gray-700 py-2 w-full text-center hover:bg-gray-100">About Us</RouterLink>
-          <RouterLink to="#products" class="text-lg text-gray-700 py-2 w-full text-center hover:bg-gray-100">Products</RouterLink>
-          <RouterLink to="#contact-us" class="text-lg text-gray-700 py-2 w-full text-center hover:bg-gray-100">Contact Us</RouterLink>
+          <RouterLink to="#home" class="text-lg text-gray-700 py-2 w-full text-center hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700">Home</RouterLink>
+          <RouterLink to="#about-us" class="text-lg text-gray-700 py-2 w-full text-center hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700">About Us</RouterLink>
+          <RouterLink to="#products" class="text-lg text-gray-700 py-2 w-full text-center hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700">Products</RouterLink>
+          <RouterLink to="#contact-us" class="text-lg text-gray-700 py-2 w-full text-center hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-700">Contact Us</RouterLink>
           <div class="flex flex-col gap-2 mt-4 w-full px-4">
-            <RouterLink to="/signin" class="py-2 px-3 inline-flex items-center text-sm font-medium rounded-xl border border-gray-200 text-black hover:bg-gray-100 dark:border-neutral-700 dark:hover:bg-white/10 dark:text-white dark:hover:text-white w-full">
+            <RouterLink to="/signin" class="py-2 px-3 inline-flex items-center text-sm font-medium rounded-xl border border-gray-200 text-black hover:bg-gray-100 dark:border-neutral-700 dark:hover:bg-white/10 dark:text-white dark:hover:text-white w-full transition-transform duration-300">
               Sign in
             </RouterLink>
-            <RouterLink to="/products" class="py-2 px-3 inline-flex items-center text-sm font-medium rounded-xl border border-transparent bg-lime-400 text-black hover:bg-lime-500 transition focus:outline-none focus:bg-lime-500 w-full">
+            <RouterLink to="/products" class="py-2 px-3 inline-flex items-center text-sm font-medium rounded-xl border border-transparent bg-lime-400 text-black hover:bg-lime-500 transition-transform duration-300 focus:outline-none focus:bg-lime-500 w-full">
               Explore Now
             </RouterLink>
           </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,15 +9,13 @@ import SuperQuality from "../components/sections/SuperQuality.vue";
 </script>
 
 <template>
-  <div class="flex flex-col min-h-screen">
-    <!-- Header -->
-    <header class="fixed top-0 inset-x-0 z-50 bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700">
-      <!-- <TheHeader /> -->
-    </header>
 
-    <!-- Main Content -->
-    <main id="content" role="main" class="flex-1 pt-[59px] bg-gray-100 dark:bg-neutral-900">
-      <HomeHero />
+  <header
+  class="fixed top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700">
+  <TheHeader />
+</header>
+<main id="content" role="main" class="pt-[59px]">
+  <HomeHero />
     
       <PopularProducts />
       <SuperQuality />
@@ -27,7 +25,7 @@ import SuperQuality from "../components/sections/SuperQuality.vue";
       <Subscribe />
     </main>
 
-  </div>
+
 </template>
 
 
